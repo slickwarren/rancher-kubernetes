3f0153c9163cee5aafdcf6fdd21e783495f5bba4
Darren Shepherd <darren@rancher.com>
2016-02-20 22:46:12 -0700

    Update to use share-mnt to setup host mounts

---
+++ rancher-k8s-images/k8s/entry.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ "$1" == "kubelet" ]; then
+    /usr/bin/share-mnt /var/lib/kubelet /var/lib/docker -- $0 --stage2 "$@"
+elif [ "$1" == "--stage2" ]; then
+    #mount --rbind /host/sys /sys
+    shift 1
+    exec "$@"
+else
+    exec "$@"
+fi
