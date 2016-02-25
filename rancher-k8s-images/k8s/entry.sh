3e49550ac000d6359705334b0802b7dfeedae8e1
Darren Shepherd <darren@rancher.com>
2016-02-25 07:44:13 -0700

    Updates to Rancher for k8s 1.2

--- rancher-k8s-images/k8s/entry.sh
+++ rancher-k8s-images/k8s/entry.sh
@@ -3,7 +3,7 @@
 if [ "$1" == "kubelet" ]; then
     /usr/bin/share-mnt /var/lib/kubelet /var/lib/docker -- $0 --stage2 "$@"
 elif [ "$1" == "--stage2" ]; then
-    #mount --rbind /host/sys /sys
+    mount --rbind /host/sys /sys
     shift 1
     exec "$@"
 else
