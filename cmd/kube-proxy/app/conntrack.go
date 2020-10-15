k3s-io/k3s

If you can't set hashsize on nf_conntrack don't fail

cmd/kube-proxy/app/conntrack.go
@@ -81,7 +81,10 @@ func (rct realConntracker) SetMax(max int) error {
 	}
 	// TODO: generify this and sysctl to a new sysfs.WriteInt()
 	glog.Infof("Setting conntrack hashsize to %d", max/4)
-	return writeIntStringFile("/sys/module/nf_conntrack/parameters/hashsize", max/4)
+	if err := writeIntStringFile("/sys/module/nf_conntrack/parameters/hashsize", max/4); err != nil {
+		glog.Errorf("failed to set conntrack hashsize to %d: %v", max/4, err)
+	}
+	return nil
 }
 
 func (rct realConntracker) SetTCPEstablishedTimeout(seconds int) error {

slickwarren/rancher-kubernetes

cmd/kube-proxy/app/conntrack.go
@@ -81,7 +81,10 @@ func (rct realConntracker) SetMax(max int) error {
 	}
 	// TODO: generify this and sysctl to a new sysfs.WriteInt()
 	klog.Infof("Setting conntrack hashsize to %d", max/4)
-	return writeIntStringFile("/sys/module/nf_conntrack/parameters/hashsize", max/4)
+	if err := writeIntStringFile("/sys/module/nf_conntrack/parameters/hashsize", max/4); err != nil {
+		klog.Errorf("failed to set conntrack hashsize to %d: %v", max/4, err)
+	}
+	return nil
 }
 
 func (rct realConntracker) SetTCPEstablishedTimeout(seconds int) error {